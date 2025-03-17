251eed88414d3d00cb816c6f0da312817b9010ad
Annushka20 <anna.khachatryan40@edu.ysu.am>
2025-03-17 17:42:22 +0400

    verjin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import React, { useState } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import { FaEyeSlash } from "react-icons/fa";
 import { IoEyeSharp } from "react-icons/io5";
-import Validation from './Validation/validation';
+import Validation from "./Validation/validation";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   const [showPass, setShowPass] = useState(false);
@@ -15,65 +14,55 @@ const App = () => {
   };
 
   return (
-    <div className='holder'>
-      <div className='text-part1'>
-        <h1 className='facebook'>facebook</h1>
-        <h2 className='facebook-2'>
-          Facebook помогает вам всегда оставаться на связи и общаться со своими знакомыми.
+    <div className="holder">
+      <div className="text-part1">
+        <h1 className="facebook">facebook</h1>
+        <h2 className="facebook-2">
+          Facebook помогает вам всегда оставаться на связи и общаться со своими
+          знакомыми.
         </h2>
       </div>
 
       <Formik
         initialValues={{
-          login: '',
-          email: '',
-          password: '',
+          login: "",
+          email: "",
+          password: "",
         }}
         validationSchema={Validation}
         onSubmit={(values) => {
-          alert('Form submitted!');
+          alert("Form submitted!");
           console.log(values);
         }}
       >
-        {({ values, handleChange, handleSubmit, errors, touched }) => (
-          <form onSubmit={handleSubmit} className='form-box'>
-            <input
-              type='text'
-              name='login'
-              placeholder='Login'
-              value={values.login}
-              onChange={handleChange}
-            />
-            {errors.login && touched.login && <p className='error'>{errors.login}</p>}
+        {
+          <Form>
+            <Field placeholder="Login" name="login" />
+            <ErrorMessage name="login" />
 
-            <input
-              type='email'
-              name='email'
-              placeholder='Email'
-              value={values.email}
-              onChange={handleChange}
-            />
-            {errors.email && touched.email && <p className='error'>{errors.email}</p>}
+            <Field placeholder="Email" name="email" />
+            <ErrorMessage name="email" />
 
-            <div className='password-box'>
-              <input
-                type={showPass ? 'text' : 'password'}
-                name='password'
-                placeholder='Password'
-                value={values.password}
-                onChange={handleChange}
+            <div className="password-box">
+              <Field
+                placeholder="Password"
+                name="password"
+                type={showPass ? "text" : "password"}
               />
+              <ErrorMessage name="password" />
+
               {showPass ? (
-                <IoEyeSharp className='eye-icon' onClick={togglePassword} />
+                <IoEyeSharp className="eye-icon" onClick={togglePassword} />
               ) : (
-                <FaEyeSlash className='eye-icon' onClick={togglePassword} />
+                <FaEyeSlash className="eye-icon" onClick={togglePassword} />
               )}
             </div>
-            {errors.password && touched.password && <p className='error'>{errors.password}</p>}
-
-            <button className='enter' type='submit'>Enter</button>
-          </form>
-        )}
+            <ErrorMessage name="password" />
+            <button className="enter" type="submit">
+              Enter
+            </button>
+          </Form>
+        }
       </Formik>
     </div>
   );
